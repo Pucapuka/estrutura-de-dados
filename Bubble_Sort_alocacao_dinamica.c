08762af4bd7bb40ae9c5098cfd9e82ee46732e54
Paulo Lima <102117492+Pucapuka@users.noreply.github.com>
2023-08-20 19:25:11 -0300

    Update Bubble_Sort_alocacao_dinamica.c
    ainda em edição. Não está funcionando

--- Bubble_Sort_alocacao_dinamica.c
+++ Bubble_Sort_alocacao_dinamica.c
@@ -1,130 +1,148 @@
-/******************************************************************************
-
-Bubble Sort - Alocação Dinâmica
-
-*******************************************************************************/
+/****************************************************
+Praticando Bubble Sort
+*****************************************************/ 
 #include <stdio.h>
 #include <stdlib.h>
-#define size 5
+#include <stdbool.h>
 
-struct Vet{
-  int tamanho;
-  int dado;
-  int primeiro;
-  int ultimo
-  int numItens; 
+typedef struct No {
+    int dado;
+    struct No* proximo;
+} No;
+
+typedef struct Vet {
+    No* posterior;
+    No* anterior;
+} Vet;
+
+Vet* criarVetor() {
+    Vet* vetor = (Vet*)malloc(sizeof(Vet));
+    vetor->posterior = NULL;
+    vetor->anterior = NULL;
+    return vetor;
 }
 
-void criarVetor(struct Vet *v, int t){
-    v-> tamanho = t;
-    v-> dado = (int*)malloc(v->tamanho*sizeof(int));
-    v-> primeiro = 0;
-    v-> ultimo = -1;
-    v-> numItens = 0;
-}
-
-void adicionarItemAoVetor(struct Vet *v, int d){
-    if(v-> ultimo == v->tamanho-1);
-        v-> ultimo = -1;
-        
-    v-> ultimo ++;
-    v-> dado[v->ultimo] = d;
-    f-> numItens ++;
-}
-
-void estaVazia(struct Vet *v){
-    return (v-> numItens==0);
-}
-
-void estaCheia(struct Vet *v){
-    return (v-> numItens== f->tamanho);
-}
-
-void listarItens(struct Vet *v){
-    int cont, i;
+void adicionarItem(Vet* item, int valor) {
+    No* novo = (No*)malloc(sizeof(No));
+    novo->dado = valor;
     
-    for (cont=0, i= v-> primeiro; cont< v-> itens; cont++){
-        printf("%d ", v-> dado[i++]);
-        
-        if(i == v-> tamanho)
-            i=0;
+    if (item->posterior == NULL) {  // Fila vazia
+        item->posterior = novo;
+        item->anterior = novo;
+        novo->proximo = novo;  // Circular
+    } else {
+        novo->proximo = item->posterior;
+        item->anterior->proximo = novo;
+        item->anterior = novo;
     }
-    printf("\n\n");
     
+    printf("O valor %d foi adicionado com sucesso.\n", valor);
 }
 
+int removerItem(Vet* item) {
+    if (item->posterior == NULL) {
+        printf("O vetor está vazio.\n");
+        return -1;
+    }
+    
+    int valor_removido;
+    
+    if (item->posterior == item->anterior) {  // Último elemento no vetor
+        valor_removido = item->posterior->dado;
+        free(item->posterior);
+        item->posterior = NULL;
+        item->anterior = NULL;
+    } else {
+        No* temp = item->posterior;
+        valor_removido = temp->dado;
+        item->posterior = item->posterior->proximo;
+        item->anterior->proximo = item->posterior;
+        free(temp);
+    }
+    
+    printf("O valor %d foi removido do vetor.\n", valor_removido);
+    return valor_removido;
+}
 
-void bubbleSort (struct Vet *v){
-    for(v-> primeiro; v->ultimo == NULL ; v->proximo){
-        for (j=0; j<size-1; j++){
-            if(vet[j]>vet[j+1]){
-                aux = vet[j+1];
-                vet[j+1] = vet[j];
-                vet[j] = aux;
-            }
-        }
+void listar(Vet* item) {
+    if (item->posterior == NULL) {
+        printf("Não há elementos no vetor.\n");
+        return;
+    }
+    
+    No* temp = item->posterior;
+    
+    printf("\nItens no vetor: \n");
+    do {
+        printf("%d ", temp->dado);
+        temp = temp->proximo;
+    } while (temp != item->posterior);
+    
+    printf("\n");
+}
+
+void bubbleSort(Vet* item){
+    int aux;
+    No* temp = item->posterior;
+    while ((temp != item->posterior) && (item->posterior->dado > item->anterior->dado)){
+        aux = item->posterior->dado;
+        item->posterior->dado = item->anterior->dado;
+        item->anterior->dado = aux;
+    }
+    printf(item->anterior->dado);
+    printf(item->posterior->dado);
+    listar(item);
+}
+
+// Função principal
+int main() {
+    int opcao, valor;
+    Vet *item = criarVetor();
+
+    while (true) {
+        // Menu de opções
+        printf("\n------------------------------------\n");
+        printf("Ordenação de elementos - Bubble Sort");
+        printf("\n------------------------------------\n");
+        printf("\nOpções:\n");
         
-    }    
-}
+        printf("0. Sair\n");
+        printf("1. Inserir item\n");
+        printf("2. Remover item\n");
+        printf("3. Listar fila\n");
+        printf("4. Ordenar (Bubble Sort)\n");
+        
+        scanf("%d", &opcao);
 
-int main (){
-    
-    int opcao, tam, valor;
-	struct Vet vetor;
+        //trabalhando as opções uma a uma
+        switch (opcao){
+            case 1:
+                printf("Adicionar o item: ");
+                scanf("%d", &valor);
+                adicionarItem(item, valor);
+                break;
 
-	// cria o vetor
-	printf("\nTamanho do vetor? ");
-	scanf("%d",&tam);
-	criarVetor(&vetor, tam);
-   
-   while(1){ 
-    //menu
-    printf
-    ("\n-------------\nBubble Sort\n-------------\n Escolha uma opção\n");
-    printf("\nMenu:\n1 - Inserir item ao Vetor\n2 - Listar Vetor\n 3- Ordenar (Bubble Sort) 0 - Sair\n\nEscolha uma opcao: ");
-		scanf("%d", &opcao);
-	
-	switch(opcao){
+            case 2:
+                printf("Removendo...\n");
+                removerItem(item);
+                break;
 
-			case 0: exit(0);
+            case 3:
+                listar(item);
+                break;
 
-			case 1: // insere elemento
-				if (estaCheia(&vetor)){
-
-					printf("\nArray Cheia!!!\n\n");
-
-				}
-				else {
-
-					printf("\nValor do elemento a ser inserido? ");
-					scanf("%f", &valor);
-					adicionarItemAoVetor(&vetor,valor);
-
-				}
-
-				break;
-
-			case 2: // listar
-				
-					if (estaVazia(&vetor)){
-
-						printf("\nArray vazia!!!\n\n");
-
-					}
-					else {
-
-						printf("\nConteudo do vetor:\n");
-						listarItens(&vetor);
-
-					}
-					break;
-
-				default:
-					printf("\nOpcao Invalida\n\n");
-
-		}
-   }
-  
-
-  return 0;
+            case 4:
+                bubbleSort(item);
+                break;
+            
+            case 0:
+                printf("Saindo...");
+                exit(0);
+            
+            default:
+                printf("Opção inválida");
+                break;
+        }
+    }
+    return 0;
 }
