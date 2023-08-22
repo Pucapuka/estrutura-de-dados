4fc209913abdd4875167715db33a57c5f2189957
Paulo Lima <102117492+Pucapuka@users.noreply.github.com>
2023-08-22 15:31:57 -0300

    Update Bubble_Sort_alocacao_dinamica.c
    Algoritmo de ordenação Bubble Sort com Alocação dinâmica.

--- Bubble_Sort_alocacao_dinamica.c
+++ Bubble_Sort_alocacao_dinamica.c
@@ -3,153 +3,63 @@ Praticando Bubble Sort
 *****************************************************/ 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
-typedef struct No {
-    int dado;
-    struct No* proximo;
-} No;
+// Definindo a estrutura
+struct Elemento {
+    int valor;
+};
 
-typedef struct Vet {
-    No* posterior;
-    No* anterior;
-} Vet;
-
-Vet* criarVetor() {
-    Vet* vetor = (Vet*)malloc(sizeof(Vet));
-    vetor->posterior = NULL;
-    vetor->anterior = NULL;
-    return vetor;
+// Função para trocar dois elementos
+void trocar(struct Elemento *a, struct Elemento *b) {
+    struct Elemento temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-void adicionarItem(Vet* item, int valor) {
-    No* novo = (No*)malloc(sizeof(No));
-    novo->dado = valor;
-    
-    if (item->posterior == NULL) {  // Fila vazia
-        item->posterior = novo;
-        item->anterior = novo;
-        novo->proximo = novo;  // Circular
-    } else {
-        novo->proximo = item->posterior;
-        item->anterior->proximo = novo;
-        item->anterior = novo;
-    }
-    
-    printf("O valor %d foi adicionado com sucesso.\n", valor);
-}
-
-int removerItem(Vet* item) {
-    if (item->posterior == NULL) {
-        printf("O vetor está vazio.\n");
-        return -1;
-    }
-    
-    int valor_removido;
-    
-    if (item->posterior == item->anterior) {  // Último elemento no vetor
-        valor_removido = item->posterior->dado;
-        free(item->posterior);
-        item->posterior = NULL;
-        item->anterior = NULL;
-    } else {
-        No* temp = item->posterior;
-        valor_removido = temp->dado;
-        item->posterior = item->posterior->proximo;
-        item->anterior->proximo = item->posterior;
-        free(temp);
-    }
-    
-    printf("O valor %d foi removido do vetor.\n", valor_removido);
-    return valor_removido;
-}
-
-void listar(Vet* item) {
-    if (item->posterior == NULL) {
-        printf("Não há elementos no vetor.\n");
-        return;
-    }
-    
-    No* temp = item->posterior;
-    
-    printf("\nItens no vetor: \n");
-    do {
-        printf("%d ", temp->dado);
-        temp = temp->proximo;
-    } while (temp != item->posterior);
-    
-    printf("\n");
-}
-
-void bubbleSort(Vet* item){
-    int i,j;
-    if (item -> posterior == NULL){
-        printf("\nLista vazia\n");
-        return;
-    }
-    No* n = (No*)malloc(sizeof(No));
-    No* aux = NULL;
-    
-    for(item->anterior; item->posterior=NULL; item->posterior->proximo){
-        if(item->posterior->dado<item->anterior->dado){
-        aux = item->posterior;
-        item->posterior = item->anterior;
-        item->anterior = aux;
-        free(aux);
+// Função de ordenação Bubble Sort
+void bubbleSort(struct Elemento *array, int tamanho) {
+    int i, j;
+    for (i = 0; i < tamanho - 1; i++) {
+        for (j = 0; j < tamanho - i - 1; j++) {
+            if (array[j].valor > array[j + 1].valor) {
+                trocar(&array[j], &array[j + 1]);
+            }
         }
- 
     }
 }
 
-// Função principal
 int main() {
-    int opcao, valor;
-    Vet *item = criarVetor();
-
-    while (true) {
-        // Menu de opções
-        printf("\n------------------------------------\n");
-        printf("Ordenação de elementos - Bubble Sort");
-        printf("\n------------------------------------\n");
-        printf("\nOpções:\n");
-        
-        printf("0. Sair\n");
-        printf("1. Inserir item\n");
-        printf("2. Remover item\n");
-        printf("3. Listar fila\n");
-        printf("4. Ordenar (Bubble Sort)\n");
-        
-        scanf("%d", &opcao);
-
-        //trabalhando as opções uma a uma
-        switch (opcao){
-            case 1:
-                printf("Adicionar o item: ");
-                scanf("%d", &valor);
-                adicionarItem(item, valor);
-                break;
-
-            case 2:
-                printf("Removendo...\n");
-                removerItem(item);
-                break;
-
-            case 3:
-                listar(item);
-                break;
-
-            case 4:
-                bubbleSort(item);
-                break;
-            
-            case 0:
-                printf("Saindo...");
-                exit(0);
-            
-            default:
-                printf("Opção inválida");
-                break;
-        }
+    int tamanho;
+    
+    printf("Digite o tamanho do array: ");
+    scanf("%d", &tamanho);
+    
+    // Alocação dinâmica de um array de estruturas Elemento
+    struct Elemento *array = (struct Elemento *)malloc(tamanho * sizeof(struct Elemento));
+    
+    if (array == NULL) {
+        printf("Erro de alocação de memória\n");
+        return 1;
     }
+    
+    // Preencha o array com valores
+    printf("Digite os elementos do array:\n");
+    for (int i = 0; i < tamanho; i++) {
+        scanf("%d", &array[i].valor);
+    }
+
+    // Chame a função bubbleSort para ordenar o array
+    bubbleSort(array, tamanho);
+
+    // Imprima o array ordenado
+    printf("Array ordenado:\n");
+    for (int i = 0; i < tamanho; i++) {
+        printf("%d ", array[i].valor);
+    }
+    printf("\n");
+
+    // Libere a memória alocada
+    free(array);
+    
     return 0;
 }
