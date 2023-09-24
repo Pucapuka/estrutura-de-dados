a9d9986eb162e40eec3de21c33df2d3ffd40e39e
Paulo Lima <102117492+Pucapuka@users.noreply.github.com>
2023-09-24 20:01:07 -0300

    Update Insertion_sort.c
    implementado o procedimento ordenador, mas está errado.

--- Insertion_sort.c
+++ Insertion_sort.c
@@ -1,36 +1,120 @@
+// uma lista duplamente encadeada, pega do site do Wagner Gaspar (https://wagnergaspar.com/como-construir-uma-lista-duplamente-encadeada/)
 #include <stdio.h>
 #include <stdlib.h>
 
-#define size 10
+typedef struct no{
+    int valor;
+    struct no *proximo;
+    struct no *anterior;
+}No;
 
-void insertionSort(int array[], int s){
-    int i,j, current;
-    for (i=1; i<s; i++){
-        current = array[i];
-        for(j=i-1; j>=0; j--){
-            if(current<array[j]){
-                array[j+1] = array[j];
+// procedimento para inserir no início
+void inserir_no_inicio(No **lista, int num){
+    No *novo = malloc(sizeof(No));
+
+    if(novo){
+        novo->valor = num;
+        novo->proximo = *lista;
+        novo->anterior = NULL;
+        if(*lista)
+            (*lista)->anterior = novo;
+        *lista = novo;
+    }
+    else
+        printf("Erro ao alocar memoria!\n");
+}
+
+// procedimento para inserir no fim
+void inserir_no_fim(No **lista, int num){
+    No *aux, *novo = malloc(sizeof(No));
+
+    if(novo){
+        novo->valor = num;
+        novo->proximo = NULL;
+
+        // é o primeiro?
+        if(*lista == NULL){
+            *lista = novo;
+            novo->anterior = NULL;
+        }
+        else{
+            aux = *lista;
+            while(aux->proximo)
+                aux = aux->proximo;
+            aux->proximo = novo;
+            novo->anterior = aux;
+        }
+    }
+    else
+        printf("Erro ao alocar memoria!\n");
+}
+
+
+void imprimir(No *no){
+    printf("\n\tLista: ");
+    while(no){
+        printf("%d ", no->valor);
+        no = no->proximo;
+    }
+    printf("\n\n");
+}
+
+// retorna ponteiro para o último nó da lista
+No* ultimo(No **lista){
+    No *aux = *lista;
+    while(aux->proximo)
+        aux = aux->proximo;
+    return aux;
+}
+
+void insertionSort(No *no){
+    No** ultimo = ultimo(**no);
+    No* current;
+    int aux;
+    current = no->proximo;
+    for(current; current!=*ultimo; current->proximo){
+        for(no; no!=*ultimo;no->anterior){
+            if(current->valor < no->valor){
+                no->proximo = no;
             }else{
                 break;
             }
         }
-        array[j+1] = current;
+      no->proximo = current;  
     }
 }
 
 int main(){
-    int array[size] = {4,2,1,3,7,8,6,9,5,10};
-    int i;
-    printf("Before insertion sort:\n");
-    for (i = 0; i<size; i++){
-        printf("%d\n",array[i]);
-    }
-    
-    insertionSort(array, size);
-    printf("After insertion sort:\n");
-    for (i = 0; i<size; i++){
-        printf("%d\n",array[i]);
-    }
+
+    int opcao, valor, anterior;
+    No *lista = NULL;
+
+    do{
+        printf("\n\t0 - Sair\n\t1 - Inserir no Início\n\t2 - Inserir no Fim\n\t3 - Imprimir\n\t4 - Ordenar\n");
+        scanf("%d", &opcao);
+
+        switch(opcao){
+        case 1:
+            printf("Digite um valor: ");
+            scanf("%d", &valor);
+            inserir_no_inicio(&lista, valor);
+            break;
+        case 2:
+            printf("Digite um valor: ");
+            scanf("%d", &valor);
+            inserir_no_fim(&lista, valor);
+            break;
+        case 3:
+            imprimir(lista);
+            break;
+        case 4:
+            insertionSort(lista);
+        default:
+            if(opcao != 0)
+                printf("Opcao invalida!\n");
+        }
+
+    }while(opcao != 0);
 
     return 0;
 }
